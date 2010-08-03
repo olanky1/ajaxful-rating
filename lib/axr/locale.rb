f5a0df1dd07b814273d8bfcd42d27af36de281c7
Jamie Kite <jamie@izea.com>
2010-08-03 02:40:10 -0400

    Last try. -_-

--- lib/axr/locale.rb
+++ lib/axr/locale.rb
@@ -12,18 +12,17 @@ module AjaxfulRating # :nodoc:
     DEFAULTS = {
       :user_rating => "Your rating: {{value}} out of {{max}}",
       :global_average => "Global rating average: {{value}} out of {{max}}",
-      :global_average_zero => "This has not yet been rated",
       :hover => "Rate {{value}} out of {{max}}",
       :no_ratings => "Not yet rated"
     }
     
     def i18n(key, value = nil)
       key = if key == :current
-        options[:show_user_rating] ? :user_rating : (!!value ? :global_average_zero : :global_average)
+        options[:show_user_rating] ? :user_rating : :global_average
       else
         key.to_sym
       end
-      key = :no_ratings if key == :user_rating && options[:show_user_rating] && ((value || show_value) == 0)
+      key = :no_ratings if ((key == :user_rating && options[:show_user_rating]) || key == :global_average) && ((value || show_value) == 0)
       default = DEFAULTS[key]
       key = "ajaxful_rating.helper.#{key}"
       I18n.t(key, :value => (value || show_value),
